4fdcf7b675991effd9588e5da355c3c4979725e9
Solenad <roe.dizon80@gmail.com>
2026-03-18 01:52:09 +0800

    feat: added first half of the eda

--- notebooks/02-eda.py
+++ notebooks/02-eda.py
@@ -21,7 +21,6 @@ import seaborn as sns
 
 df = pd.read_csv("./data/data_processed.csv")
 df.head()
-# %%
 
 # %%
 # Data Overview and Quality
@@ -29,7 +28,6 @@ df.info()
 df.describe(include="all")
 print("Missing values per column:\n", df.isnull().sum())
 print("Duplicate rows:", df.duplicated().sum())
-# %%
 
 # %%
 # Distribution
@@ -40,5 +38,57 @@ plt.ylabel("Count")
 plt.show()
 
 df["won_first"].value_counts(normalize=True)
+
 # %%
-print(test)
+# Distribution of player ratings
+plt.figure(figsize=(8, 4))
+sns.histplot(df["rating_first"], kde=True, bins=30)
+plt.title("Distribution of Player Ratings")
+plt.xlabel("Rating")
+plt.ylabel("Frequency")
+plt.show()
+
+# %%
+# Distribution of game lengths in seconds
+plt.figure(figsize=(8, 4))
+sns.histplot(df["duration_sec"], kde=True, bins=30)
+plt.title("Distribution of Game Lengths (Seconds)")
+plt.xlabel("Game Duration (Seconds)")
+plt.ylabel("Frequency")
+plt.show()
+
+# %%
+# Distribution of pieces per second (pps) and attack per second
+plt.figure(figsize=(8, 4))
+sns.histplot(df["pps"], kde=True, color="blue", bins=30, label="Pieces per Second")
+plt.title("Distribution of Pieces per Second")
+plt.xlabel("PPS")
+plt.ylabel("Frequency")
+plt.legend()
+plt.show()
+
+plt.figure(figsize=(8, 4))
+sns.histplot(
+    df["attack_per_sec"], kde=True, color="orange", bins=30, label="Attack per Second"
+)
+plt.title("Distribution of Attack per Second")
+plt.xlabel("Attack/sec")
+plt.ylabel("Frequency")
+plt.legend()
+plt.show()
+
+# %%
+# Average attack per minute: Compare between winning and losing games
+plt.figure(figsize=(8, 4))
+sns.boxplot(
+    x="won_first",
+    y="attack_per_sec",
+    data=df,
+    hue="won_first",
+    palette="Set2",
+    dodge=False,
+)
+plt.title("Attack per Second Comparison (Win vs Loss)")
+plt.xlabel("Won (1=Win, 0=Loss)")
+plt.ylabel("Attack per Second")
+plt.show()
