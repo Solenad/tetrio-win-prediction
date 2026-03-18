d93f5a339512f0097a228f96393e52f357ce0c15
420Rain <dulatrerain@gmail.com>
2026-03-19 04:08:01 +0800

    feat: added neural network implementation

---
+++ src/data_loader.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+
+class DataLoader(object):
+
+    def __init__(self, X, y, batch_size):
+        """Class constructor for DataLoader
+
+        Arguments:
+            X {np.ndarray} -- A numpy array of shape (N, D) containing the
+            data; there are N samples each of dimension D.
+            y {np.ndarray} -- A numpy array of shape (N, 1) containing the
+            ground truth values.
+            batch_size {int} -- An integer representing the number of instances
+            per batch.
+        """
+        self.X = X
+        self.y = y
+        self.batch_size = batch_size
+
+        self.indices = np.array([i for i in range(self.X.shape[0])])
+        np.random.seed(1)
+
+    def shuffle(self):
+        """Shuffles the indices in self.indices.
+        """
+
+        np.random.shuffle(self.indices)
+
+    def get_batch(self, mode='train'):
+        """Returns self.X and self.y divided into different batches of size
+        self.batch_size according to the shuffled self.indices.
+
+        Arguments:
+            mode {str} -- A string which determines the mode of the model. This
+            can either be `train` or `test`.
+
+        Returns:
+            list, list -- List of np.ndarray containing the data divided into
+            different batches of size self.batch_size; List of np.ndarray
+            containing the ground truth labels divided into different batches
+            of size self.batch_size
+        """
+
+        X_batch = []
+        y_batch = []
+
+        if mode == 'train':
+            self.shuffle()
+        elif mode == 'test':
+            self.indices = np.array([i for i in range(self.X.shape[0])])
+
+        for i in range(0, len(self.indices), self.batch_size):
+
+            if i + self.batch_size <= len(self.indices):
+                indices = self.indices[i:i + self.batch_size]
+
+            else:
+                indices = self.indices[i:]
+
+            X_batch.append(self.X[indices])
+            y_batch.append(self.y[indices])
+
+        return X_batch, y_batch
